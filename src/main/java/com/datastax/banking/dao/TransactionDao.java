fedca32d46e73f4e9f0cec8cbc93363a69319286
Patrick Callaghan <patcho@gmail.com>
2016-10-18 18:11:27 +0100

    updated for new methods

--- src/main/java/com/datastax/banking/dao/TransactionDao.java
+++ src/main/java/com/datastax/banking/dao/TransactionDao.java
@@ -51,11 +51,11 @@ public class TransactionDao {
 			+ latestTransactionTable
 			+ " (cc_no, transaction_time, transaction_id, location, merchant, amount, user_id, status, notes, tags) values (?,?,?,?,?,?,?,?,?,?) ";
 
-	private static final String GET_TRANSACTIONS_BY_ID = "select * from " + transactionTable
-			+ " where cc_no = ? and year = ?";
+	private static final String GET_LATEST_TRANSACTIONS_BY_CCNO = "select * from " + latestTransactionTable
+			+ " where cc_no = ?";
 	private static final String GET_TRANSACTIONS_BY_CCNO = "select * from " + transactionTable
 			+ " where cc_no = ? and year = ? and transaction_time >= ? and transaction_time < ?";
-	private static final String GET_LATEST_TRANSACTIONS_BY_CCNO = "select * from " + latestTransactionTable
+	private static final String GET_LATEST_TRANSACTIONS_BY_CCNO_DATE = "select * from " + latestTransactionTable
 			+ " where cc_no = ? and transaction_time >= ? and transaction_time < ?";
 
 	private PreparedStatement insertTransactionStmt;
@@ -63,6 +63,8 @@ public class TransactionDao {
 	private PreparedStatement getTransactionById;
 	private PreparedStatement getTransactionByCCno;
 	private PreparedStatement getLatestTransactionByCCno;
+	private PreparedStatement getLatestTransactionByCCnoDate;
+	
 	private MovingAverage ma = new MovingAverage(50);
 
 	private AtomicLong count = new AtomicLong(0);
@@ -98,9 +100,9 @@ public class TransactionDao {
 			this.insertTransactionStmt = session.prepare(INSERT_INTO_TRANSACTION);
 			this.insertLatestTransactionStmt = session.prepare(INSERT_INTO_LATEST_TRANSACTION);
 
-			this.getTransactionById = session.prepare(GET_TRANSACTIONS_BY_ID);
-			this.getTransactionByCCno = session.prepare(GET_TRANSACTIONS_BY_CCNO);
 			this.getLatestTransactionByCCno = session.prepare(GET_LATEST_TRANSACTIONS_BY_CCNO);
+			this.getTransactionByCCno = session.prepare(GET_TRANSACTIONS_BY_CCNO);
+			this.getLatestTransactionByCCnoDate = session.prepare(GET_LATEST_TRANSACTIONS_BY_CCNO_DATE);
 
 			this.insertLatestTransactionStmt.setConsistencyLevel(ConsistencyLevel.LOCAL_QUORUM);
 			this.insertTransactionStmt.setConsistencyLevel(ConsistencyLevel.LOCAL_QUORUM);
@@ -211,6 +213,11 @@ public class TransactionDao {
 		t.setTags(row.getSet("tags", String.class));
 
 		return t;
+	}
+
+	public List<Transaction> getLatestTransactionsForCCNo(String ccNo) {
+		ResultSet resultSet = this.session.execute(getLatestTransactionByCCno.bind(ccNo));
+		return processResultSet(resultSet, null);
 	}
 
 	public List<Transaction> getLatestTransactionsForCCNoTagsAndDate(String ccNo, Set<String> tags, DateTime from,
