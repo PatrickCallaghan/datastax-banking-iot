d210962e44f5a0fe1f06d072f080d0652b23642d
Patrick Callaghan <patcho@gmail.com>
2016-10-18 19:19:12 +0100

    updated with new retry

--- src/main/java/com/datastax/banking/dao/TransactionDao.java
+++ src/main/java/com/datastax/banking/dao/TransactionDao.java
@@ -1,4 +1,4 @@
-package com.datastax.banking.dao;
+	package com.datastax.banking.dao;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.MetricRegistry;
+import com.datastax.banking.MyConstantSpeculativeExecutionPolicy;
 import com.datastax.banking.data.TransactionGenerator;
 import com.datastax.banking.model.Transaction;
 import com.datastax.demo.utils.MovingAverage;
@@ -25,7 +26,6 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.ResultSetFuture;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
-import com.datastax.driver.core.policies.ConstantSpeculativeExecutionPolicy;
 
 /**
  * Inserts into 2 tables
@@ -76,8 +76,8 @@ public class TransactionDao {
 
 	public TransactionDao(String[] contactPoints) {
 
-		ConstantSpeculativeExecutionPolicy policy =
-			    new ConstantSpeculativeExecutionPolicy(5,3);
+		MyConstantSpeculativeExecutionPolicy policy =
+			    new MyConstantSpeculativeExecutionPolicy(5,3);
 		
 		cluster = Cluster.builder()
 				.addContactPoints(contactPoints)
